e5c0d9387511de6768a6e3115b33d31c38207ef0
Michael Salerno <msalerno1993@gmail.com>
2023-10-28 15:29:53 -0400

    starting mongoDB creation of database

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require('mongoose')
 
 const app = require("./app");
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require("./models/planets.models");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://msalerno1993:ApexLegends93@cluster0.lpfzz5l.mongodb.net/nasa?retryWrites=true&w=majority"
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready');
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: false
+  })
   await loadPlanetsData();
 
   server.listen(PORT, () => {
