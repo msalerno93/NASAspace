3b7d8b93101a87f10d1b6aa2d33bed94f6196a8e
Michael Salerno <msalerno1993@gmail.com>
2023-10-28 16:10:06 -0400

    ensure connection to db from backend

--- server/src/server.js
+++ server/src/server.js
@@ -15,13 +15,12 @@ mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready');
 })
 
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+})
+
 async function startServer() {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: false
-  })
+  await mongoose.connect(MONGO_URL)
   await loadPlanetsData();
 
   server.listen(PORT, () => {
